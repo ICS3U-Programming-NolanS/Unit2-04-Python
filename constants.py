44a646f9164804640bbd740e52d6771201d59e3a
Nolan Shami <nolan.shami@stu.ocsb.ca>
2022-10-13 18:17:45 +0000

    Added constants.py and pizza_cost.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created by: Nolan Shami
+# Created on: Ocotober 10th, 2022
+# This program contains constants.
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.5
